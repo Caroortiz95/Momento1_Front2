abad45df43cc56c6d3ed993e2234503420f175d0
caroortiz95 <caro.ortiz9501@gmail.com>
2026-07-01 19:46:15 -0500

    Se agrega la gestión de estado en los botones

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ( { textoBoton }) => {
+const Button = ( { textoBoton, onClick, colorFondo }) => {
   return (
-    <button className="bg-[#1F5E4A] text-white rounded-full px-8 py-3 font-semibold hover:bg-[#1B4A3A] transition">
+    <button onClick={onClick} className={`${colorFondo} hover:brightness-90 text-white rounded-full px-8 py-3 font-semibold transition`}>
         {textoBoton}
     </button>
   );
--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import Button from "./Button"
 
 const MenuCard = ({ codigo, nombre, descripcion, precio }) => {
+    const [agregar, setAgregar] = useState(0);
     return (
 
         <article className="bg-white rounded-2xl border border-[#DED6C5] p-6 hover:-translate-y-1 hover:shadow-xl transition-all">
@@ -8,7 +10,15 @@ const MenuCard = ({ codigo, nombre, descripcion, precio }) => {
             <h2 className="text-2xl font-body font-semibold text-[#15201B]">{nombre}</h2>
             <p className="font-body text-xl  text-[#15201B]">{descripcion}</p>
             <p className="font-mono text-[#1F5E4A] font-semibold">{precio}</p>
-            <Button textoBoton="Agregar al pedido" />
+            <Button textoBoton="Agregar al pedido" colorFondo="bg-[#1F5E4A]"
+                onClick={(agregar) => setAgregar((agregar) => agregar + 1)} />
+            {agregar > 0 && (
+                <>
+                    <p className="font-mono text-[#15201B] font-semibold"> Cantidad: {agregar}</p>
+
+                    <Button textoBoton="Quitar" colorFondo="bg-[#E8643C]" onClick={() => setAgregar((agregar) => agregar - 1)}></Button>
+                </>)}
+
         </article>
     )
 }
